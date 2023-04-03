2675ee005db66ad85103f3f5d4eecbb78f87efeb
thlo7777 <thlo7777@qq.com>
2023-04-03 11:11:17 +0800

    modified:   src/env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite/client" />
 
+//comments
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
